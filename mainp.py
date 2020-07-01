a2e71fb6f3d707b43a4f5f49f83f1ea21c09ee2f
VanessaFer <vanessa.ferrando@gmail.com>
2020-07-01 17:34:25 +0000

    ho creato main.py

---
+++ mainp.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def maxOfArray(arr):
+  return np.max(arr)
+def maxOfArray(listval):
+
+a = np.array([2,4,5])
+b = ["fls","fjnd",5]
+c = "casa"
+d = 
+l = (3,5)
+
+listVal = [2,45,67,89]
+maxVal = maxOfArray
+
+print (a.shape)
